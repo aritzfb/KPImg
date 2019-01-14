0e8b53be6ddcfadb676d980bfb8a7fa422d8449f
FRANCOY Aritz - IBERMATICA <aritz.francoy@loreal.com>
2019-01-14 09:43:14 +0100

    changed version

--- .tmp/drop/visual.js
+++ .tmp/drop/visual.js
@@ -850,7 +850,7 @@ var powerbi;
                 name: 'kPImg0051F6D5AD8348148E01E9E4B31C9F41',
                 displayName: 'KPImg',
                 class: 'Visual',
-                version: '1.0.0',
+                version: '1.0.1',
                 apiVersion: '2.2.0',
                 create: function (options) { return new powerbi.extensibility.visual.kPImg0051F6D5AD8348148E01E9E4B31C9F41.Visual(options); },
                 custom: true
