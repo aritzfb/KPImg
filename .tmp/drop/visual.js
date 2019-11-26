e382127d8429e57a51e30e13d555cc74e3e741b5
Aritz Francoy Barreiro <aritzfb@gmail.com>
2019-11-26 21:18:20 +0100

    compiled for test

--- .tmp/drop/visual.js
+++ .tmp/drop/visual.js
@@ -1097,8 +1097,8 @@ var powerbi;
     (function (visuals) {
         var plugins;
         (function (plugins) {
-            plugins.kPImg0051F6D5AD8348148E01E9E4B31C9F41_DEBUG_DEBUG = {
-                name: 'kPImg0051F6D5AD8348148E01E9E4B31C9F41_DEBUG_DEBUG',
+            plugins.kPImg0051F6D5AD8348148E01E9E4B31C9F41_DEBUG = {
+                name: 'kPImg0051F6D5AD8348148E01E9E4B31C9F41_DEBUG',
                 displayName: 'KPImg',
                 class: 'Visual',
                 version: '1.0.2',
